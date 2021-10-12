a5190cd0a89480d336f1afb8b796acfa96870e7e
Ayoob <aa1319@ic.ac.uk>
2021-10-12 14:06:44 +0100

    [QSYNC-2] Translated JS files to TS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,146 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Modal from "./components/Modal";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+import Modal from "./components/CustomModal";
+import { createFalse } from "typescript";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface Item {
+  id: number;
+  title: string;
+  description: string;
+  completed: boolean;
 }
+
+const App = () => {
+  let defaultItem: Item = {
+    id: 1,
+    title: "",
+    description: "",
+    completed: false,
+  };
+
+  const [activeItem, setActiveItem] = useState(defaultItem);
+  const [viewCompleted, setViewCompleted] = useState(false);
+  const [todoList, setTodoList] = useState<Item[]>([]);
+  const [modal, setModal] = useState(false);
+
+  const refreshList = () => {
+    axios
+      .get("http://localhost:8000/api/todos/")
+      .then((res) => setTodoList(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  const displayCompleted = (status: boolean) => {
+    setViewCompleted(status);
+  };
+
+  const renderTabList = () => {
+    return (
+      <div className="my-5 tab-list">
+        <span
+          onClick={() => displayCompleted(true)}
+          className={viewCompleted ? "active" : ""}
+        >
+          complete
+        </span>
+        <span
+          onClick={() => displayCompleted(false)}
+          className={viewCompleted ? "" : "active"}
+        >
+          Incomplete
+        </span>
+      </div>
+    );
+  };
+
+  const renderItems = () => {
+    const newItems = todoList.filter(
+      (item) => item.completed === viewCompleted
+    );
+    return newItems.map((item) => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className={`todo-title mr-2 ${viewCompleted ? "completed-todo" : ""}`}
+          title={item.description}
+        >
+          {item.title}
+        </span>
+        <span>
+          <button
+            onClick={() => editItem(item)}
+            className="btn btn-secondary mr-2"
+          >
+            {" "}
+            Edit{" "}
+          </button>
+          <button onClick={() => handleDelete(item)} className="btn btn-danger">
+            Delete{" "}
+          </button>
+        </span>
+      </li>
+    ));
+  };
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  const handleSubmit = (item: Item) => {
+    toggle();
+    if (item.id) {
+      axios
+        .put(`http://localhost:8000/api/todos/${item.id}/`, item)
+        .then((res) => refreshList());
+      return;
+    }
+    axios
+      .post("http://localhost:8000/api/todos/", item)
+      .then((res) => refreshList());
+  };
+
+  const handleDelete = (item: Item) => {
+    axios
+      .delete(`http://localhost:8000/api/todos/${item.id}`)
+      .then((res) => refreshList());
+  };
+
+  const createItem = () => {
+    const item: Item = { title: "", description: "", completed: false, id: 0 };
+    setActiveItem(item);
+    toggle();
+  };
+
+  const editItem = (item: Item) => {
+    setActiveItem(item);
+    toggle();
+  };
+
+  return (
+    <main className="content">
+      <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
+      <div className="row ">
+        <div className="col-md-6 col-sm-10 mx-auto p-0">
+          <div className="card p-3">
+            <div className="">
+              <button onClick={createItem} className="btn btn-primary">
+                Add task
+              </button>
+            </div>
+            {renderTabList()}
+            <ul className="list-group list-group-flush">{renderItems()}</ul>
+          </div>
+        </div>
+      </div>
+      {modal ? (
+        <Modal activeItem={activeItem} toggle={toggle} onSave={handleSubmit} />
+      ) : null}
+    </main>
+  );
+};
 
 export default App;
---
+++ frontend/src/components/CustomModal.tsx
@@ -0,0 +1,89 @@
+import React, { Component, useState } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
+
+import { Item } from "../App";
+
+interface Props {
+  activeItem: Item;
+  toggle: () => void;
+  onSave: (item: Item) => void;
+}
+
+const CustomModal = (props: Props) => {
+  const [activeItem, setActiveItem] = useState<Item>(props.activeItem);
+
+  const { toggle, onSave } = props;
+  return (
+    <div>
+      <Modal isOpen={true} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Todo Item</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="todo-title">Title</Label>
+              <Input
+                type="text"
+                id="todo-title"
+                name="title"
+                value={activeItem.title}
+                onChange={(e) =>
+                  setActiveItem({ ...activeItem, title: e.currentTarget.value })
+                }
+                placeholder="Enter Todo Title"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="todo-description">Description</Label>
+              <Input
+                type="text"
+                id="todo-description"
+                name="description"
+                value={activeItem.description}
+                onChange={(e) =>
+                  setActiveItem({
+                    ...activeItem,
+                    description: e.currentTarget.value,
+                  })
+                }
+                placeholder="Enter Todo description"
+              />
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input
+                  type="checkbox"
+                  name="completed"
+                  checked={activeItem.completed}
+                  onChange={(e) =>
+                    setActiveItem({
+                      ...activeItem,
+                      completed: e.currentTarget.checked,
+                    })
+                  }
+                />
+                Completed
+              </Label>
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="success" onClick={() => onSave(props.activeItem)}>
+            Save
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default CustomModal;
