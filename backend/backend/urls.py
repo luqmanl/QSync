296afec5e8228959f3113e77dafcc16c7c1b6ae6
sanchit <sanchitajmera2017@gmail.com>
2021-10-16 22:36:35 +0100

    [QSYNC-8] Fix django imports and settings

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from ..qsync import views
+from qsync import views
 
 router = routers.DefaultRouter()
 router.register(r'qsync', views.QsyncView, 'qsync')
