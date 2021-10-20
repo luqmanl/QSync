33ae72975d9c91ef8b40caaef7eb6c17c089e21d
Mazen <mah319@ic.ac.uk>
2021-10-20 19:17:49 +0100

    [QSYNC-8] Refactor feed handler to push deltas to table. Changed table name.

--- backend/src/app/feed_handler.py
+++ backend/src/app/feed_handler.py
@@ -9,6 +9,8 @@ from qpython.qtype import QSYMBOL_LIST, QDOUBLE_LIST, QTIMESTAMP
 
 from qpython.qcollection import qlist
 
+import json
+
 # Examples of some handlers for different updates. These currently don't do much.
 # Handlers should conform to the patterns/signatures in callback.py
 # Handlers can be normal methods/functions or async. The feedhandler is paused
@@ -18,35 +20,41 @@ from qpython.qcollection import qlist
 
 async def book(book_, timestamp):
     print("\n--------\n")
-    for x in book_.book.bids:
-        print(x, book_.book.bids[x])
+    print(type(book_.delta))
 
-        q.sendAsync('.u.upd', np.string_('book'), [
+    if (book_.delta is None):
+        return
+
+    for (x, y) in book_.delta['bid']:
+        q.sendAsync('.u.upd', np.string_('orderbooktop'), [
             qlist([np.string_(book_.symbol)], qtype=QSYMBOL_LIST),
             qlist([qtemp.from_raw_qtemporal(timestamp, QTIMESTAMP)],
                   qtype=QTIMESTAMP),
             qlist(['bid'], qtype=QSYMBOL_LIST),
             qlist([x], qtype=QDOUBLE_LIST),
-            qlist([book_.book.bids[x]], qtype=QDOUBLE_LIST),
+            qlist([y], qtype=QDOUBLE_LIST),
         ])
 
-    for x in book_.book.asks:
-        q.sendAsync('.u.upd', np.string_('book'), [
+    for (x, y) in book_.delta['ask']:
+        q.sendAsync('.u.upd', np.string_('orderbooktop'), [
             qlist([np.string_(book_.symbol)], qtype=QSYMBOL_LIST),
             qlist([qtemp.from_raw_qtemporal(timestamp, QTIMESTAMP)],
                   qtype=QTIMESTAMP),
             qlist(['ask'], qtype=QSYMBOL_LIST),
             qlist([x], qtype=QDOUBLE_LIST),
-            qlist([book_.book.asks[x]], qtype=QDOUBLE_LIST),
+            qlist([y], qtype=QDOUBLE_LIST),
         ])
 
 
 def main():
-    config = {'log': {'filename': 'demo.log',
-                      'level': 'DEBUG', 'disabled': False}}
+    # demo log
+    # config = {'log': {'filename': 'demo.log',
+    #                  'level': 'DEBUG', 'disabled': False}}
     # The config will be automatically passed into any exchanges set up by string.
     # Instantiated exchange objects would need to pass the config in manually.
-    f = FeedHandler(config=config)
+    # f = FeedHandler(config=config)
+
+    f = FeedHandler()
     # pair is ['BTC-USDT']
     pairs = Binance.symbols()[10: 11]
     f.add_feed(Binance(symbols=pairs, channels=[
