2aeefee61d2ef060175bd0be1236669e603012f7
abdur <ars319@ic.ac.uk>
2021-11-05 17:03:43 +0000

    [QSYNC-21] Modified subscriber to send data to group specified by data recieved

--- backend/src/app/subscriber.py
+++ backend/src/app/subscriber.py
@@ -49,7 +49,7 @@ class ListenerThread(threading.Thread):
                         f'type: {type(message)}, message type: {message.type},')
                     data_str += (
                         f'data size: {message.size}, is_compressed: {message.is_compressed}')
-                    print(data_str)
+                    # print(data_str)
 
                     if isinstance(message.data, list) and len(
                             message.data) == 3:
@@ -59,18 +59,19 @@ class ListenerThread(threading.Thread):
                             for (time, sym, fht, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, bs1, bs2, bs3, bs4, bs5, bs6, bs7, bs8, bs9, bs10, as1, as2, as3, as4, as5, as6, as7, as8, as9, as10) in message.data[2]:
                                 data = {
                                     'time': str(time),
-                                    'sym': str(sym),
+                                    'sym': sym.decode("utf-8"),
                                     'feedhandlerTime': str(fht),
                                     'buys': [str(b1), str(b2), str(b3), str(b4), str(b5), str(b6), str(b7), str(b8), str(b9), str(b10)],
                                     'asks': [str(a1), str(a2), str(a3), str(a4), str(a5), str(a6), str(a7), str(a8), str(a9), str(a10)],
                                     'buy_sizes': [str(bs1), str(bs2), str(bs3), str(bs4), str(bs5), str(bs6), str(bs7), str(bs8), str(bs9), str(bs10)],
                                     'ask_sizes': [str(as1), str(as2), str(as3), str(as4), str(as5), str(as6), str(as7), str(as8), str(as9), str(as10)]
                                 }
-                                print(data)
+                                # print(data)
+
+                                group_name = f"binance_{(sym).decode('utf-8')}_orderbook"
 
                                 async_to_sync(
-                                    channel_layer.group_send)(
-                                    "qsync", {
+                                    channel_layer.group_send)(group_name, {
                                         "type": "send_tick_data", "text": json.dumps(data)})
                 except QException as e:
                     print(e)
