5fd67fc91013e023b9d2069088a84d3707b26082
D, Tyrell <tyrell.duku19@imperial.ac.uk>
2021-11-15 16:23:06 +0000

    [QSYNC-24] Removed direct any types from SearchBar

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,43 +1,48 @@
-import React, { useState } from 'react'
-import './SearchBar.css'
+import React, { useState } from "react";
+import "./SearchBar.css";
 
-const SearchBar = () => {
-    const [query, setQuery] = useState('');
-    const exchanges = [
-        { id: 1, name: "BTC" },
-        { id: 2, name: "ETH" },
-        { id: 3, name: "DOGE" },
-    ];
-
-    const filterExchanges = (exchanges: any, query: string) => {
-        if (!query) {
-            return exchanges;
-        }
-    
-        return exchanges.filter((e: any) => {
-            const postName = e.name.toLowerCase();
-            return postName.includes(query);
-        });
-    };
-
-    const filteredExchanges = filterExchanges(exchanges, query);
-    
-    return (
-        <div className ='search-bar'>
-            <input
-                type='search'
-                placeholder='Select an option...'
-                value={query} onInput={e => setQuery((e.target as HTMLInputElement).value)}
-            />
-            <ul>
-                {filteredExchanges.map((e: any) => (
-                    <li  className='list_element' key={e.id}>
-                        {e.name}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+type queryType = {
+  id: number,
+  name: string
 }
+const SearchBar = () => {
+  const [query, setQuery] = useState("");
+  const exchanges: queryType[] = [
+    { id: 1, name: "BTC" },
+    { id: 2, name: "ETH" },
+    { id: 3, name: "DOGE" },
+  ];
+
+  const filterExchanges = (exchanges: queryType[], query: string) => {
+    if (!query) {
+      return exchanges;
+    }
+
+    return Object.values(exchanges).filter((e) => {
+      const postName = e.name.toLowerCase();
+      return postName.includes(query);
+    });
+  };
+
+  const filteredExchanges = filterExchanges(exchanges, query);
+
+  return (
+    <div className="search-bar">
+      <input
+        type="search"
+        placeholder="Select an option..."
+        value={query}
+        onInput={(e) => setQuery((e.target as HTMLInputElement).value)}
+      />
+      <ul>
+        {filteredExchanges.map((e) => (
+          <li className="list_element" key={e.id}>
+            {e.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default SearchBar;
