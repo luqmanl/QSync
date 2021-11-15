04a3bdc97807a38d3ae55240c753bae7e82c8716
D, Tyrell <tyrell.duku19@imperial.ac.uk>
2021-11-15 14:34:18 +0000

    [QSYNC-24] Formatting changes for Rectangle + SearchBar

--- frontend/src/components/Rectangle.tsx
+++ frontend/src/components/Rectangle.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import './Rectangle.css'
-import Dropdown from 'react-bootstrap/Dropdown'
+import React from "react";
+import "./Rectangle.css";
+import Dropdown from "react-bootstrap/Dropdown";
 const Rectangle = () => {
-    return (
-        <div className='rectangle-container' >
-            <div className='dropdown-button'>
-            <Dropdown className='dropdown'>
-                    <Dropdown.Toggle className='dropdown-toggle'
-                        style={{ background: 'black', borderColor: 'black'}}
-                        variant="primary" />
-                     <Dropdown.Menu>
-                            <Dropdown.Item href="#">Options</Dropdown.Item>
-                    </Dropdown.Menu>
-            </Dropdown>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="rectangle-container">
+      <div className="dropdown-button">
+        <Dropdown className="dropdown">
+          <Dropdown.Toggle
+            className="dropdown-toggle"
+            style={{ background: "black", borderColor: "black" }}
+            variant="primary"
+          />
+          <Dropdown.Menu>
+            <Dropdown.Item href="#">Options</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </div>
+  );
+};
 
 export default Rectangle;
