11392772aeefe89ba1dffd8c069aeb326e3405c0
abdur <ars319@ic.ac.uk>
2021-11-28 20:18:51 +0000

    [QSYNC-35] Added routes to all the consumer classes

--- backend/src/router/routing.py
+++ backend/src/router/routing.py
@@ -2,8 +2,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.conf.urls import url
 from django.urls import path
 from channels.security.websocket import AllowedHostsOriginValidator
-from app.consumers import ClientConsumer
+from app.consumers import L2orderbookConsumer, L2overviewConsumer, BasisTableConsumer, TradeTableConsumer
 
 
-ws_urlpatterns = [path(
-    'ws/data/<str:exchange_name>/<str:pair_names>/<str:data_type>/', ClientConsumer())]
+ws_urlpatterns = [path('ws/data/l2overview/', L2overviewConsumer()),
+                  path('ws/data/l2orderbook/', L2orderbookConsumer()),
+                  path('ws/data/basis/', BasisTableConsumer()),
+                  path('ws/data/trade/', TradeTableConsumer())]
