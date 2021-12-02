2117d7f2da52ccf5d48e8ad4af849eb8e38c6f2e
sanchit <sanchitajmera2017@gmail.com>
2021-12-02 04:23:48 +0000

    [QSYNC-42] Created API for historical basis data

--- backend/src/app/urls.py
+++ backend/src/app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('historicalBasisData', views.getHistoricalBasisData, name='historicalBasisData'),
+
 ]
--- backend/src/app/views.py
+++ backend/src/app/views.py
@@ -1,6 +1,13 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+import json
+from django.views.decorators.csrf import csrf_exempt,csrf_protect
 
 
 def index(request):
     return render(request, "index.html")
+
+@csrf_exempt
+def getHistoricalBasisData(request):
+    data = {"data":"this is teh data"}
+    return HttpResponse(json.dumps(data), content_type="application/json")
