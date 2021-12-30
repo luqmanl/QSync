f1e56092a9e03be5d39b4d7eab8262db54ebfff7
Ayoob <aa1319@ic.ac.uk>
2021-12-30 13:30:03 +0000

    [QSYNC-52] Removed landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-magic-numbers */
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
-import BasisTable from "./components/BasisTable";
-import RealTimeCandleSticksChart from "./components/RealTimeCandleSticksChart";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavBar from "./components/NavBar";
-import LatestTrades from "./components/LatestTrades";
-import CurrencyPair from "./components/CurrencyPair";
-import { Form } from "react-bootstrap";
-import Rectangle from "./components/Rectangle";
+import { Switch, Route } from "react-router-dom";
+import Overview from "./pages/Overview";
 
 const App = () => {
-  const [showLatest, setShowLatest] = useState(false);
 
   return (
     <div className="page">
-      <NavBar />
-      <div className="flex-container">
-        <Rectangle innerComponents={<CurrencyPair />} />
-        <Rectangle innerComponents={<BasisTable />} />
-      </div>
+      <Switch>
+        <Route path="/overview">{<Overview />}</Route>
+      </Switch>
     </div>
   );
 };
