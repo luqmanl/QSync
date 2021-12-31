a075d28f226bfc3541a037200d745f0a7b35b0d7
Ayoob <aa1319@ic.ac.uk>
2021-12-31 19:08:33 +0000

    [QSYNC-55] Finished top currency table

--- frontend/src/components/TopCurrencyTable.tsx
+++ frontend/src/components/TopCurrencyTable.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable no-magic-numbers */
 import React, { useState } from "react";
 import { Table } from "react-bootstrap";
-import "./TopCurrencyTable.css"
+import "./TopCurrencyTable.css";
 
-type tableRep = { [name: string]: responseType };
+export type tableRep = { [name: string]: responseType };
 
 interface responseType {
   name: string;
@@ -37,6 +38,44 @@ const tableColumns = [
   "Market Cap",
 ];
 
+interface colour {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const maxGreen: colour = { red: 32, green: 156, blue: 5 };
+const maxRed: colour = { red: 255, green: 10, blue: 10 };
+const gradient: colour = { red: 222, green: 156, blue: 5 };
+
+const generateColour7d = (value: number): colour => {
+  if (value >= 5) {
+    return maxGreen;
+  }
+  if (value <= -5) {
+    return maxRed;
+  }
+  return {
+    red: 143.5 - ((gradient.red / 10) * value),
+    green: 80 + ((gradient.green / 10) * value),
+    blue: 7.5 + ((gradient.blue / 10) * value ),
+  };
+};
+
+const generateColour24h = (value: number): colour => {
+  if (value >= 10) {
+    return maxGreen;
+  }
+  if (value <= -10) {
+    return maxRed;
+  }
+  return {
+    red: 143.5 - ((gradient.red / 20) * value),
+    green: 80 + ((gradient.green / 20) * value),
+    blue: 7.5 + ((gradient.blue / 20) * value ),
+  };
+};
+
 const TopCurrencyTable = () => {
   const endPoint = "/ws/data/top_currencies_table";
   const ws = new WebSocket(
@@ -62,19 +101,33 @@ const TopCurrencyTable = () => {
         <thead>
           <tr>
             {tableColumns.map((col) => {
-              return <th key={col}>col</th>;
+              return <th key={col}>{col}</th>;
             })}
           </tr>
         </thead>
         <tbody>
           {Object.entries(tableData).map(([name, data]) => {
+            const colour24h = generateColour24h(data.change24h);
+            const colour7d = generateColour7d(data.change7d);
             return (
               <tr key={name}>
                 <td>{data.name}</td>
-                <td>{data.price}</td>
-                <td>{data.change24h}</td>
-                <td>{data.change7d}</td>
-                <td>{data.marketCap}</td>
+                <td>${data.price.toLocaleString("en-UK")}</td>
+                <td
+                  style={{
+                    color: `rgb(${colour24h.red},${colour24h.green},${colour24h.blue})`,
+                  }}
+                >
+                  {data.change24h}%
+                </td>
+                <td
+                  style={{
+                    color: `rgb(${colour7d.red},${colour7d.green},${colour24h.blue})`,
+                  }}
+                >
+                  {data.change7d}%
+                </td>
+                <td>${data.marketCap.toLocaleString("en-UK")}</td>
               </tr>
             );
           })}
