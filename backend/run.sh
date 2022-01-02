0cd7b2a063d00383a97ed6c5d241de7c599ffc63
Luqman <lml19@ic.ac.uk>
2022-01-02 20:40:35 +0000

    [QSYNC-16] Added redis server launch to backend run script

--- backend/run.sh
+++ backend/run.sh
@@ -1,6 +1,7 @@
+redis-server &
+echo "Redis server started."
+
 python3 src/manage.py runserver &
 echo "Python server started."
-python3 src/app/feed_handler.py
-echo "Python feed handler started."
 
 wait $PID
