1ec3c668165ffaee244b21d0c7f896d5aef6e4f7
abdur <ars319@ic.ac.uk>
2022-01-02 14:23:59 +0000

    [QSYNC-48] Introduce mock QConnection objects for API tests

--- backend/src/app/tests/test_urls.py
+++ backend/src/app/tests/test_urls.py
@@ -2,11 +2,14 @@ from django.http import response
 from django.test.testcases import SimpleTestCase
 from django.urls import reverse, resolve
 from app.views import getHistoricalBasisData
+from mock import patch
 
 
 class TestUrls(SimpleTestCase):
-    def test_historical_basis_data_resolves(self):
+    @patch('app.views.QConnection')
+    def test_historical_basis_data_resolves(self, mock_q):
+        mock_q.sendSync.return_value = '[[1.0,2.0,3.0], [4.0,5.0,6.0]]'
         url = reverse('historicalBasisData')
-        self.assertEquals(resolve(url).func, getHistoricalBasisData)
+        self.assertEqual(resolve(url).func, getHistoricalBasisData)
         response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
--- backend/src/app/tests/test_views.py
+++ backend/src/app/tests/test_views.py
@@ -1,8 +1,13 @@
 from django.test.testcases import SimpleTestCase
 from django.urls.base import reverse
+from mock import patch
+from app.views import getHistoricalBasisData
 
 
 class TestViews(SimpleTestCase):
-    def test_historical_basis_data(self):
+    @patch('app.views.QConnection')
+    def test_historical_basis_data(self, mock_q):
+        mock_q.sendSync.return_value = '[[1.0,2.0,3.0], [4.0,5.0,6.0]]'
         url = reverse('historicalBasisData')
         response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
