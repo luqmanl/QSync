ebcef116063eee7a50f264f772ef18c28e6c25e8
Ayoob <aa1319@ic.ac.uk>
2022-01-03 20:37:42 +0000

    [QSYNC-64] Added name map to make life easier

---
+++ frontend/src/NameMap.tsx
@@ -0,0 +1,7 @@
+export const nameMap: { [name: string]: string } = {
+  "BTC-USDT": "Bitcoin",
+  "ETH-USDT": "Etheruem",
+  "ADA-USDT": "Cardano",
+  "SOL-USDT": "Solana",
+  "DOGE-USDT": "Dogecoin",
+};
