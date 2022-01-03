6d2ea6b30de40f5098bca58d33346bc7325670a0
Mustafa Ilyas <mi819@ic.ac.uk>
2022-01-03 17:38:41 +0100

    [QSYNC-58] Fixed formatting

--- frontend/src/components/NewsFeed.tsx
+++ frontend/src/components/NewsFeed.tsx
@@ -13,7 +13,9 @@ export interface newsListing {
 }
 
 const NewsFeed = () => {
-  const url = `http://${process.env.PUBLIC_URL || "localhost:8000"}/api/data/newsfeed`;
+  const url = `http://${
+    process.env.PUBLIC_URL || "localhost:8000"
+  }/api/data/newsfeed`;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [loading, setLoading] = useState(true);
   const [news, setNews] = useState<newsListing[]>([]);
@@ -23,11 +25,16 @@ const NewsFeed = () => {
       .get(url)
       .then((resp) => {
         resp.data.newsListings.forEach((elem: newsListing) => {
-          const timeDiffMins = Math.floor((new Date().getTime() - new Date(elem.timestamp).getTime()) / (1000 * 60));
-          if (timeDiffMins < 60){
+          const timeDiffMins = Math.floor(
+            (new Date().getTime() - new Date(elem.timestamp).getTime()) /
+              (1000 * 60)
+          );
+          if (timeDiffMins < 60) {
             elem.timestamp = `${timeDiffMins.toString()} mins ago`;
           } else {
-            elem.timestamp = `${Math.floor(timeDiffMins / 60)} hour${(timeDiffMins < 120 ? "" : "s")} ago`;
+            elem.timestamp = `${Math.floor(timeDiffMins / 60)} hour${
+              timeDiffMins < 120 ? "" : "s"
+            } ago`;
           }
         });
 
