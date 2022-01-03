e1a0bbe88791e80380a99a3ccfc4dbffbf97b7ed
Ayoob <aa1319@ic.ac.uk>
2022-01-03 20:38:09 +0000

    [QSYNC-64] Added analysis landing page to project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Switch, Route } from "react-router-dom";
 import Overview from "./pages/Overview";
 import Analysis from "./pages/Analysis";
+import AnalaysisLanding from "./pages/AnalaysisLanding";
 
 const App = () => {
   return (
     <div className="page">
       <Switch>
         <Route path="/overview">{<Overview />}</Route>
+        <Route exact path="/analysis">{<AnalaysisLanding />}</Route>
         <Route path="/analysis/:pair">{<Analysis />}</Route>
       </Switch>
     </div>
