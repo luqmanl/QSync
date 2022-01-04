de8ec058360dedc5f6a0d09d0408444fe780b46d
Mazen <mah319@ic.ac.uk>
2022-01-04 17:41:07 +0000

    [QSYNC-54] Add http request for getting 24 percentage change

--- backend/src/app/urls.py
+++ backend/src/app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('historicalBasisData/<str:period>/<str:spotSym>/<str:futureSym>/<str:spotExch>/<str:futureExch>', views.getHistoricalBasisData,
          name='historicalBasisData'),
-
+    path('historical24hChangeData', views.getHistorical24hChangeData,
+         name='historical24hChangeData'),
 ]
