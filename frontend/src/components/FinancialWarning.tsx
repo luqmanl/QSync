2c04817db16e4a41a22dca879eeb170552d262cd
Ayoob <aa1319@ic.ac.uk>
2022-01-04 23:00:16 +0000

    [QSYNC-70] Added cookies to make the user look at the warning

---
+++ frontend/src/components/FinancialWarning.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+
+interface propType {
+  first: boolean;
+}
+
+const FinancialWarning = (props: propType) => {
+  const [show, setShow] = useState(true);
+  const { first } = props;
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  return (
+    <div>
+      <Modal
+        show={first && show}
+        onHide={() => {
+          handleClose();
+        }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Financial Warning</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            The Content is for informational purposes only, you should not
+            construe any such information or other material as legal, tax,
+            investment, financial, or other advice. Please seek advice for any
+            investments
+          </p>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default FinancialWarning;
