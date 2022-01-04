e330c7cbab8f71d3efcf032633900a0d0a848eec
Ayoob <aa1319@ic.ac.uk>
2022-01-04 14:34:51 +0000

    [QSYNC-64] Removed coin summary as it is no longer useful

--- frontend/src/CoinData.tsx
+++ frontend/src/CoinData.tsx
@@ -5,11 +5,3 @@ export const nameMap: { [name: string]: string } = {
   "SOL-USDT": "Solana",
   "DOGE-USDT": "Dogecoin",
 };
-
-export const coinSummary: { [name: string]: string } = {
-  "BTC-USDT": `Bitcoin is a decentralized cryptocurrency originally described in a 2008 whitepaper by a person, or group of people, using the alias Satoshi Nakamoto. It was launched soon after, in January 2009.
-
-  Bitcoin is a peer-to-peer online currency, meaning that all transactions happen directly between equal, independent network participants, without the need for any intermediary to permit or facilitate them. Bitcoin was created, according to Nakamoto’s own words, to allow “online payments to be sent directly from one party to another without going through a financial institution.”
-  
-  Some concepts for a similar type of a decentralized electronic currency precede BTC, but Bitcoin holds the distinction of being the first-ever cryptocurrency to come into actual use.`,
-};
