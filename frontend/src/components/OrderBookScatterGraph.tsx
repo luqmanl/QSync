587cc239f9daab1512ec66943d61a1789a0aa902
Ayoob <aa1319@ic.ac.uk>
2022-01-05 21:23:40 +0000

    [QSYNC-72] Added orderbook scatter graph

---
+++ frontend/src/components/OrderBookScatterGraph.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { bookData } from './OrderBookTable'
+
+
+
+const OrderBookScatterGraph = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default OrderBookScatterGraph
