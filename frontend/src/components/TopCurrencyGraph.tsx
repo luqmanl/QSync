6df4323d01acaf34d86b356f022f0d5a315f7ec4
Ayoob <aa1319@ic.ac.uk>
2022-01-05 00:39:07 +0000

    [QSYNC-54] Attempted to fix problem

--- frontend/src/components/TopCurrencyGraph.tsx
+++ frontend/src/components/TopCurrencyGraph.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable new-cap */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-magic-numbers */
-import React, { useEffect,useState } from "react";
-import exampleData from "../exampleData/ExampleTopCurrencyGraphData";
-import { LegendBoxBuilders, lightningChart, Themes, LineSeries } from "@arction/lcjs";
+import React, { useEffect, useState } from "react";
+import { LegendBoxBuilders, lightningChart, Themes } from "@arction/lcjs";
 import "./TopCurrencyGraph.css";
-import axios from "axios"
+import axios from "axios";
 export interface data {
   data: dataPoint[];
 }
@@ -22,70 +23,75 @@ interface graphPoint {
 // const colours = ["red", "blue", "green", "pink", "brown"];
 
 const TopCurrencyGraph = () => {
-  const historicalAddr = `http://${process.env.back || "localhost:8000"}/historical24hChangeData`
-  const [stuf, setStuf] = useState<{[name : string] : graphPoint[]}>({})
+  const historicalAddr = `http://${
+    process.env.back || "localhost:8000"
+  }/historical24hChangeData`;
 
-  
-  
   useEffect(() => {
-    const map: { [name: string]: graphPoint[] } = {}
-    axios.get(historicalAddr).then((res) => {
-      const { points } = res.data;
-      const pointList = points as dataPoint[]
-      pointList.forEach((obj)=>{
-        const newPoint = {x : new Date(obj.timestamp).getTime(), y : obj.percentage}
-        if (obj.currency in map){
-          map[obj.currency].push(newPoint)
-          
-        } else {
-          map[obj.currency] = [newPoint]
-        }
+    const map: { [name: string]: graphPoint[] } = {};
+    axios
+      .get(historicalAddr)
+      .then((res) => {
+        const { points } = res.data;
+        const pointList = points as dataPoint[];
+        pointList.forEach((obj) => {
+          const newPoint = {
+            x: new Date(obj.timestamp).getTime() * (60 * 24),
+            y: obj.percentage * 100,
+          };
+          if (obj.currency in map) {
+            map[obj.currency].push(newPoint);
+          } else {
+            map[obj.currency] = [newPoint];
+          }
+        });
       })
-      setStuf(map)
-    }).catch((err) => { console.log(err, historicalAddr) })
-}, [])
+      .catch((err) => {
+        console.log(err, historicalAddr);
+      });
+    const chart = lightningChart().ChartXY({
+      theme: Themes.lightNew,
+      container: "currency-graph",
+    });
+    chart.setTitle("Top Currencies");
+    chart.getDefaultAxisX().setTitle("Time");
+    chart.getDefaultAxisY().setTitle("Percentage Change");
 
-useEffect(()=>{
-  const chart = lightningChart().ChartXY({
-    theme: Themes.lightNew,
-    container: "currency-graph",
-  })
-  chart.setTitle("Top Currencies");
-  chart.getDefaultAxisX().setTitle("Time");
-  chart.getDefaultAxisY().setTitle("Percentage Change");
+    const entries = Object.entries(map);
+    const names = entries.map(([a, _b]) => a);
+    const lists = entries.map(([_, b]) => b);
 
-  const seriesMap : {[cur : string] : LineSeries} = {}
+    const seriesArray = new Array(5).fill(null).map((_, idx) =>
+      chart
+        .addLineSeries({
+          dataPattern: {
+            pattern: "ProgressiveX",
+          },
+        })
+        // eslint-disable-next-line arrow-parens
+        .setStrokeStyle((stroke) => stroke.setThickness(1))
+        .setName(names[idx])
+    );
 
+    seriesArray.forEach((series, idx) => {
+      if (idx === 1) {
+        series.add(lists[idx]);
+      }
+    });
 
-  const entries = Object.entries(stuf)
-  const names = entries.map(([a,_])=> a)
-  const lists = entries.map(([_,b])=>b)
+    chart.addLegendBox(LegendBoxBuilders.HorizontalLegendBox).add(chart);
 
-  const seriesArray = new Array(5).fill(null).map((_,idx) => chart
-    .addLineSeries({ dataPattern: {
-      pattern: 'ProgressiveX',
-    } })
-      .setStrokeStyle(stroke => stroke.setThickness(1))
-      .setName(names[idx])
-  )
+    return () => {
+      chart.dispose();
+    };
+  }, []);
 
-  seriesArray.forEach((series,idx)=>{
-    series.add(lists[idx])
-  })
-
- 
-
-  chart.addLegendBox(LegendBoxBuilders.HorizontalLegendBox).add(chart);
-  
-  console.log(seriesArray)
-},[stuf])
-
-// done thnx
-return (
-  <div className="graph-container">
-    <div id="currency-graph" className="graph-container"></div>
-  </div>
-);
+  // done thnx
+  return (
+    <div className="graph-container">
+      <div id="currency-graph" className="graph-container"></div>
+    </div>
+  );
 };
 
 export default TopCurrencyGraph;
