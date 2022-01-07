c807d723c75790685701139c78799260ce7d2fbe
Ayoob <aa1319@ic.ac.uk>
2022-01-07 13:17:04 +0000

    [QSYNC-72] Basis structure of page finished

--- frontend/src/pages/Arbitrage.tsx
+++ frontend/src/pages/Arbitrage.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./Arbitrage.css";
+import SideBar from "../components/SideBar";
 
 const Arbitrage = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="page-box">
+      <div className="arbitrage-title-box">
+        <SideBar addr="arbitrage" />
+        <h2 className="arbitrage-title">Arbitrage</h2>
+      </div>
+      <div className="arbitrage-container">
+        <div className="arbitrage-column"></div>
+        <div className="arbitrage-column"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Arbitrage
+export default Arbitrage;
