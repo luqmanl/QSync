f7708aa09becab15663a61f4274d5797963d2c3a
Ayoob <aa1319@ic.ac.uk>
2022-01-07 13:17:39 +0000

    [QSYNC-72] Linting fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import Overview from "./pages/Overview";
 import Analysis from "./pages/Analysis";
 import AnalysisLanding from "./pages/AnalysisLanding";
-import Arbitrage from "./pages/Arbitrage"
+import Arbitrage from "./pages/Arbitrage";
 const App = () => {
   return (
     <div className="page">
